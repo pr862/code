481bdc6c6314476c23515c4135c2f95b52cdfa19
pr862 <princyvithani09@gmail.com>
2025-07-02 15:38:13 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const button = document.getElementById('clickBtn');
+
+  button.addEventListener('click', function () {
+    alert('Button clicked! Welcome to GitHub Pages!');
+  });
+});
